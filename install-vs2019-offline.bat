811a66ee089829156d6896f5db27597238dcf29d
shea-c4 <github.9yqb4@simplelogin.com>
2025-04-27 21:48:09 -0400

    add install-vs2019-offline.bat script for offline installation of Visual Studio 2019

---
+++ install-vs2019-offline.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set vsoDir=d:\vs2019offline
+if "%1" neq "" set vsoDir=%~1
+
+echo %~n0: vsoDir=%vsoDir%
+
+pushd "%vsoDir%"
+.\vs_setup.exe --noWeb --passive --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --includeRecommended
+popd
